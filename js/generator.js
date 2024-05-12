136ee6054901ebfd6af942d664325f7ebd06358e
Ben.Sullivan <ben@bens-MacBook-Air.local>
2024-05-11 21:18:25 -0500

    Remove entropy, style the page

--- js/generator.js
+++ js/generator.js
@@ -6,63 +6,58 @@ const CHAR_SET = {
     Special: "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 }
 
-//Calculate the selected password's entropy
-function calculateEntropy(length, chars){
-    return Math.log2(Math.pow(chars.length, length)).toFixed(2)
-}
-
-//Returns a string from the selected character set. 
-function generatePassword(length, characters){
-    if(length > 256){
+//Returns a string from the selected character set.
+function generatePassword(length, characters) {
+    if (length > 256) {
         return "Password length is too long."
     }
 
     var array = ""
-    
-    for(var i = 0; i < length; i++)
-        array += characters[generateRandomInt(0, characters.length-1)]
+
+    for (var i = 0; i < length; i++)
+        array += characters[generateRandomInt(0, characters.length - 1)]
 
     return array
 }
 
 //Cryptographically secure random number generator for grabbing individual chars
-function generateRandomInt(min, max){
+function generateRandomInt(min, max) {
     var range = max - min + 1
     var max_range = CHAR_SET.Numbers.length + CHAR_SET.Lower.length + CHAR_SET.Upper.length + CHAR_SET.Special.length
     var array = new Uint8Array(1)
 
     window.crypto.getRandomValues(array)
 
-    if(array[0] >= Math.floor(max_range/range) * range)
+    if (array[0] >= Math.floor(max_range / range) * range)
         return generateRandomInt(min, max)
     return min + (array[0] % range)
 }
 
 //Grabs the selected characters from the checkboxes
-function getCharArray(){
+function getCharArray() {
     var selected_chars = ""
     //Character logic
-    if($("#lower_box").is(":checked"))
+    if ($("#lower_box").is(":checked"))
         selected_chars += CHAR_SET.Lower
-    if($("#upper_box").is(":checked"))
+    if ($("#upper_box").is(":checked"))
         selected_chars += CHAR_SET.Upper
-    if($("#num_box").is(":checked"))
+    if ($("#num_box").is(":checked"))
         selected_chars += CHAR_SET.Numbers
-    if($("#spec_box").is(":checked"))
+    if ($("#spec_box").is(":checked"))
         selected_chars += CHAR_SET.Special
     //If nothing is selected behave as if everything is.
-    if(!selected_chars)
+    if (!selected_chars)
         selected_chars += CHAR_SET.Lower + CHAR_SET.Upper + CHAR_SET.Numbers + CHAR_SET.Special
 
     return selected_chars
 }
 
 //Grabs the user input length
-function getPwLength(){
+function getPwLength() {
     var selected_length = parseInt($("#length_box").val())
 
     //Length logic
-    if(!(1 <= selected_length && selected_length <=256)){
+    if (!(1 <= selected_length && selected_length <= 256)) {
         $("#password_results").text("Please pick a number between 1-256.")
         return
     }
@@ -70,7 +65,6 @@ function getPwLength(){
     return selected_length
 }
 
-$("#results_button").click("click", function(){
+$("#results_button").click("click", function () {
     $("#password_results").text(generatePassword(getPwLength(), getCharArray()))
-    $("#password_entropy").text(calculateEntropy(getPwLength(), getCharArray()))
 })
